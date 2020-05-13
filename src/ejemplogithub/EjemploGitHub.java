669c8b4a8a40985f06e7868d2abcb0d53a8a72ef
Alexi <Alexi@DESKTOP-JJMJS27>
2020-05-13 12:30:00 -0600

    Primer commit del proyecto

---
+++ src/ejemplogithub/EjemploGitHub.java
@@ -0,0 +1,12 @@
+//Ejemplo creado para subir a GitHub
+package ejemplogithub;
+
+public class EjemploGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos...!");
+        System.out.println("Welcome");
+        System.out.println("Hi...!!!");
+    }
+    
+}
