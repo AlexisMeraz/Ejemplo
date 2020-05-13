5db7c7b957f63b9cc0fac7d6104377ba66563579
Alexi <Alexi@DESKTOP-JJMJS27>
2020-05-13 12:51:54 -0600

    Primer commit del proyecto

--- src/ejemplogithub/EjemploGitHub.java
+++ src/ejemplogithub/EjemploGitHub.java
@@ -6,7 +6,8 @@ public class EjemploGitHub {
     public static void main(String[] args) {
         System.out.println("Bienvenidos...!");
         System.out.println("Welcome");
-        System.out.println("Hi...!!!");
+        System.out.println("Hi...!!!");//primera actualizacion de netbeans
+        
     }
     
 }
